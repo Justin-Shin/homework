72395b0f9417c3b9d4619779a9742297d992b8db
Justin Shin <jjs3b@virginia.edu>
2017-05-05 18:46:26 -0400

    need to add submit functionality

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -1,13 +1,41 @@
-function Household {
+var addBtn = document.querySelector(".add")
+var sbmtBtn = document.querySelector("[type=submit]")
+var hshldObj = new Household()
+
+function Household() {
+  // this will hold each family member's information as an object
   this.members = {}
+  // Need this to create keys for object members
+  this.num_members = 0
   this.addMember = function(member){
-
+    this.members[this.num_members] = member
+    appendFamily(member,this.num_members)
+    this.num_members++
   }
-  this.deleteMember = function(member){
-
+  this.deleteMember = function(number){
+    delete this.members[number]
   }
 }
 
+function appendFamily(family, number){
+  // Create the list item to append to the OL (that is already in HTML)
+  var li = document.createElement("li")
+  li.appendChild(document.createTextNode(`Age: ${family.age}; Relationship: ${family.relationship}; Smoker: ${family.smoker} `))
+  // li.setAttribute("class",`delete-${number}`)
+  var liMade = document.querySelector("ol").appendChild(li)
+
+  var button = document.createElement("button")
+  button.appendChild(document.createTextNode("delete"))
+  // button.setAttribute("class",`delete-${number}`)
+  var buttonMade = liMade.appendChild(button)
+
+  buttonMade.addEventListener('click', function(event){
+    event.preventDefault()
+    document.querySelector("ol").removeChild(li);
+    hshldObj.deleteMember(number)
+  })
+
+}
 
 // This function should be invoked to get data that is inside the
 // form and return it, so any changes are always apparent
@@ -41,10 +69,6 @@ function verifyData(data){
   }
 }
 
-var addBtn = document.querySelector(".add")
-var sbmtBtn = document.querySelector("[type=submit]")
-var hshldObj = New Household()
-
 // Listener for clicking add, should check check for validity, get
 // data, and if valid add to the Household.
 addBtn.addEventListener('click', function(event){
@@ -53,9 +77,11 @@ addBtn.addEventListener('click', function(event){
   var dataVerified = verifyData(getFormData())
   if(dataVerified){
     //data is valid
+    console.log(dataVerified)
     hshldObj.addMember(dataVerified)
   } else {
     //means data is not valid
+    alert("Please enter valid data!")
   }
 });
 
@@ -64,5 +90,4 @@ addBtn.addEventListener('click', function(event){
 sbmtBtn.addEventListener('click', function(event){
   //prevent reload
   event.preventDefault()
-
 });
